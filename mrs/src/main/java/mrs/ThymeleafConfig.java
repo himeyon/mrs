7657eae7c9bc84829e4224a6d7f55881cd3ae1d4
himeyon <mizuki15@gmail.com>
2017-10-02 11:32:32 +0900

    thymeleaf 3 -> 2

--- mrs/src/main/java/mrs/ThymeleafConfig.java
+++ mrs/src/main/java/mrs/ThymeleafConfig.java
@@ -7,10 +7,10 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 @Configuration
 public class ThymeleafConfig {
 
-/*
+
 	@Bean
 	public Java8TimeDialect java8TimeDialect() {
 		return new Java8TimeDialect();
 	}
-*/
+
 }
